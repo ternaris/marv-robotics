eb5944d9e55f01687aaf53992bc975c2cac5047a
Florian Friesdorf <florian@ternaris.com>
2018-04-11 17:09:26 +0200

    [marv] Support webapi extension via entry points

--- code/marv/marv_webapi/__init__.py
+++ code/marv/marv_webapi/__init__.py
@@ -5,6 +5,8 @@
 
 from __future__ import absolute_import, division, print_function
 
+from pkg_resources import iter_entry_points
+
 from .auth import auth
 from .comment import comment
 from .dataset import dataset
@@ -31,3 +33,8 @@ webapi.add_endpoint(tag)
 
 from marv_robotics.webapi import robotics
 webapi.add_endpoint(robotics)
+
+
+for ep in iter_entry_points(group='marv_webapi'):
+    endpoint = ep.load()
+    webapi.add_endpoint(endpoint)
