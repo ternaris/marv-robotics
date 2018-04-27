1ac1e116dc7f5d4f085ab65b1789dc24f76eb212
Florian Friesdorf <florian@ternaris.com>
2018-04-28 00:22:12 +0200

    [docs] Add note about styling for custom widgets

--- docs/config/custom.js
+++ docs/config/custom.js
@@ -4,37 +4,37 @@
  */
 
 window.marv_extensions = {
-    formats: {
-        // replace default datetime formatter, show times in UTC
-        'datetime': function(date) { return new Date(date).toUTCString(); }
-    },
-    widgets: {
-        // rowcount widget displays the number of rows in a table
-        'rowcount': [
-            /* insert callback, renders the data
+  formats: {
+    // replace default datetime formatter, show times in UTC
+    'datetime': function(date) { return new Date(date).toUTCString(); }
+  },
+  widgets: {
+    // rowcount widget displays the number of rows in a table
+    'rowcount': [
+      /* insert callback, renders the data
 
-               @function insert
-               @param {HTMLElement} element The parent element
-               @param {Object} data The data to be rendered
-               @return {Object} state Any variable, if required by remove
-            */
-            function insert(element, data) {
-                const doc = element.ownerDocument;
-                const el = doc.createTextNode(data.rows.length + ' rows');
-                element.appendChild(el);
+        @function insert
+        @param {HTMLElement} element The parent element
+        @param {Object} data The data to be rendered
+        @return {Object} state Any variable, if required by remove
+      */
+      function insert(element, data) {
+        const doc = element.ownerDocument;
+        const el = doc.createTextNode(data.rows.length + ' rows');
+        element.appendChild(el);
 
-                const state = { el };
-                return state;
-            },
+        const state = { el };
+        return state;
+      },
 
-            /* remove callback, clean up if necessary
+      /* remove callback, clean up if necessary
 
-               @function remove
-               @param {Object} state The state object returned by insert
-            */
-            function remove(state) {
-                state.el.remove();
-            }
-        ]
-    }
+        @function remove
+        @param {Object} state The state object returned by insert
+      */
+      function remove(state) {
+        state.el.remove();
+      }
+    ]
+  }
 };
