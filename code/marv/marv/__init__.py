606367fcda7f1d744bb80edc4e07bd3c77324e4b
Florian Friesdorf <florian@ternaris.com>
2020-01-28 16:27:54 +0100

    Deprecate marv.api_endpoint and marv.api_group

--- code/marv/marv/__init__.py
+++ code/marv/marv/__init__.py
@@ -1,6 +1,7 @@
 # Copyright 2016 - 2018  Ternaris.
 # SPDX-License-Identifier: AGPL-3.0-only
 
+from marv_api import deprecation
 from marv_node.io import Abort
 from marv_node.io import create_group
 from marv_node.io import create_stream
@@ -13,13 +14,16 @@ from marv_node.io import push
 from marv_node.io import set_header
 from marv_node.node import input, node
 from marv_node.tools import select
-from marv_webapi.tooling import api_endpoint
-from marv_webapi.tooling import api_group
+from marv_webapi.tooling import api_endpoint as _api_endpoint
+from marv_webapi.tooling import api_group as _api_group
+
+DEPRECATIONS = {
+    'api_endpoint': deprecation.Info(__name__, '20.07', _api_endpoint),
+    'api_group': deprecation.Info(__name__, '20.07', _api_group),
+}
 
 __all__ = [
     'Abort',
-    'api_endpoint',
-    'api_group',
     'create_group',
     'create_stream',
     'get_logger',
@@ -33,3 +37,5 @@ __all__ = [
     'select',
     'set_header',
 ]
+
+__dir__, __getattr__ = deprecation.dir_and_getattr(__name__, __all__, DEPRECATIONS)
