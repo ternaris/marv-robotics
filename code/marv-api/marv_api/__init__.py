6768f11ecd504e630a2d6916fd69efd82d1f9521
Florian Friesdorf <florian@ternaris.com>
2020-01-28 18:40:10 +0100

    Make all marv.io controls available via marv_api

--- code/marv-api/marv_api/__init__.py
+++ code/marv-api/marv_api/__init__.py
@@ -1,13 +1,35 @@
 # Copyright 2016 - 2020  Ternaris.
 # SPDX-License-Identifier: AGPL-3.0-only
 
+from marv_node.io import (
+    Abort,
+    create_group,
+    create_stream,
+    get_logger,
+    get_requested,
+    make_file,
+    pull,
+    pull_all,
+    push,
+    set_header,
+)
 from .decorators import input, node, select
 from .scanner import DatasetInfo
 
 
 __all__ = (
+    'Abort',
     'DatasetInfo',
+    'create_group',
+    'create_stream',
+    'get_logger',
+    'get_requested',
     'input',
+    'make_file',
     'node',
+    'pull',
+    'pull_all',
+    'push',
     'select',
+    'set_header',
 )
--- code/marv/marv/__init__.py
+++ code/marv/marv/__init__.py
@@ -3,16 +3,6 @@
 
 import marv_api
 from marv_api import deprecation
-from marv_node.io import Abort
-from marv_node.io import create_group
-from marv_node.io import create_stream
-from marv_node.io import get_logger
-from marv_node.io import get_requested
-from marv_node.io import make_file
-from marv_node.io import pull
-from marv_node.io import pull_all
-from marv_node.io import push
-from marv_node.io import set_header
 from marv_webapi.tooling import api_endpoint as _api_endpoint
 from marv_webapi.tooling import api_group as _api_group
 
@@ -26,23 +16,21 @@ DEPRECATIONS.update(
         __name__, '20.07', getattr(marv_api, name),
         f'Use marv_api.{name} instead: import marv_api as marv.',
     )) for name in (
+        'Abort',
+        'create_group',
+        'create_stream',
+        'get_logger',
+        'get_requested',
         'input',
+        'make_file',
         'node',
+        'pull',
+        'pull_all',
+        'push',
         'select',
+        'set_header',
     )
 )
 
-__all__ = [
-    'Abort',
-    'create_group',
-    'create_stream',
-    'get_logger',
-    'get_requested',
-    'make_file',
-    'pull',
-    'pull_all',
-    'push',
-    'set_header',
-]
-
+__all__ = ()
 __dir__, __getattr__ = deprecation.dir_and_getattr(__name__, __all__, DEPRECATIONS)
