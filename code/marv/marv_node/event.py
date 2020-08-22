7f04a51c3eca3db082ad2412913dc932ae5bf6b8
Florian Friesdorf <florian@ternaris.com>
2020-08-22 17:42:30 +0200

    Address new linter warnings

--- code/marv/marv_node/event.py
+++ code/marv/marv_node/event.py
@@ -7,12 +7,12 @@ from collections import OrderedDict
 
 class DefaultOrderedDict(OrderedDict):
     def __init__(self, factory):
-        super(DefaultOrderedDict, self).__init__()
+        super().__init__()
         self._factory = factory
 
     def __getitem__(self, key):
         try:
-            return super(DefaultOrderedDict, self).__getitem__(key)
+            return super().__getitem__(key)
         except KeyError:
             value = self._factory()
             self[key] = value
