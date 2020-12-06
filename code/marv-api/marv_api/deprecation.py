08718ce949e7f80b0cbe39c3eba4446133c6d72d
Florian Friesdorf <florian@ternaris.com>
2020-12-06 22:22:53 +0100

    Add decorator to declare function deprecated

--- code/marv-api/marv_api/deprecation.py
+++ code/marv-api/marv_api/deprecation.py
@@ -1,6 +1,7 @@
 # Copyright 2020  Ternaris.
 # SPDX-License-Identifier: AGPL-3.0-only
 
+import functools
 import warnings
 from dataclasses import dataclass
 from typing import Any
@@ -30,3 +31,18 @@ def make_getattr(module, dct):
         return info.obj
 
     return __getattr__
+
+
+def deprecated(version, msg=None, name=None):
+    """Wrap function to trigger deprecated message upon call."""
+    def deco(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kw):
+            _msg = (
+                f'{func.__module__}.{name or func.__name__} will be removed in {version}; '
+                f'{msg or "please let us know if this is an issue for you."}'
+            )
+            warnings.warn(_msg, FutureWarning, stacklevel=2)
+            return func(*args, **kw)
+        return wrapper
+    return deco
