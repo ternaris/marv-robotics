c31d579f346884df6430f52a78ef56033db5fa5c
Peter Kessen <p.kessen@kalverkamp.de>
2021-05-17 14:52:52 +0000

    Implemented filter for untagged bags

--- code/marv/marv/db.py
+++ code/marv/marv/db.py
@@ -1132,7 +1132,7 @@ class Database:
                 x['value'] for x in await txn.exq(Query.from_(desc.table).select('value'))
             ]
             for desc in descs
-            if {'any', 'all', 'none'}.intersection(collection.filter_specs[desc.key].operators)
+            if {'any', 'all', 'none', 'untagged'}.intersection(collection.filter_specs[desc.key].operators)
         }
         all_known.update({
             'f_status': list(STATUS),
@@ -1225,6 +1225,12 @@ class Database:
                                                         .join(tag)
                                                         .on(dataset_tag.tag_id == tag.id)
                                                         .where(tag.value.isin(value))
+                                                        .select(dataset_tag.dataset_id)
+                                                        .distinct()))
+                elif operator == 'untagged':
+                    query = query.where(listing.id.notin(Query.from_(dataset_tag)
+                                                        .join(tag, JoinType.left_outer)
+                                                        .on(dataset_tag.tag_id == tag.id)
                                                         .select(dataset_tag.dataset_id)
                                                         .distinct()))
 
